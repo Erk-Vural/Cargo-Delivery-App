343afc9cf13653db53ac0d5970bd1ceb4c49da7b
Erk-Vural <erk.vural@gmail.com>
2021-10-24 22:22:13 +0300

    0.3.0 - Shortest path and related issues(Green)

--- src/renderer/map.ts
+++ src/renderer/map.ts
@@ -5,8 +5,18 @@ let map: google.maps.Map;
 
 const additionalOptions = {};
 
+// Direction Options
+let directionsDisplay: google.maps.DirectionsRenderer;
+const directionsService = new google.maps.DirectionsService();
+let size = 0;
+let currentPosition;
+
+// An array to store results from Google routing API.
+let routeResults: any[] = [];
+
 // Load map
 let markers: google.maps.Marker[] = [];
+const places: { latLng: any }[] = [];
 
 const startPoint = { lat: 47.701, lng: -122.36 };
 
@@ -18,10 +28,14 @@ const loader = new Loader({
 
 // InitMap()
 loader.load().then(() => {
+  directionsDisplay = new google.maps.DirectionsRenderer();
+
   map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
     center: startPoint,
     zoom: 8,
   });
+
+  directionsDisplay.setMap(map);
 
   // load  and update markers to mapWindow
   ipcRenderer.on("marker:list", (err, cargos) => {
@@ -30,14 +44,21 @@ loader.load().then(() => {
     cargos.forEach((element: any) => {
       const cargo = element._doc;
 
-      if (!cargo.delivered) {
-        const latLang = {
-          lat: Number(cargo.locationX),
-          lng: Number(cargo.locationY),
-        };
+      const latLang = {
+        lat: Number(cargo.locationX),
+        lng: Number(cargo.locationY),
+      };
 
-        addMarker(latLang, false);
-      }
+      const place = {
+        title: cargo.clientName,
+        latLng: new google.maps.LatLng(
+          Number(cargo.locationX),
+          Number(cargo.locationY)
+        ),
+      };
+
+      places.push(place);
+      addMarker(latLang, false);
     });
     // Adds a marker at the center of the map.
     addMarker(startPoint, true);
@@ -49,9 +70,67 @@ loader.load().then(() => {
 
     ipcRenderer.send("click:addMarker", latLng);
   });
+
+  findNearestPlace();
 });
 
-// Utils
+// Direction Utils
+
+// Loops through all inteesting places to calculate route between our current position
+// and that place.
+function findNearestPlace() {
+  let i = places.length;
+  size = places.length;
+  routeResults = [];
+  while (i--) {
+    calcRoute(places[i].latLng, storeResult);
+  }
+}
+
+// A function to calculate the route between our current position and some desired end point.
+function calcRoute(
+  end: any,
+  callback: { (data: any): void; (arg0: google.maps.DirectionsResult): void }
+) {
+  const request = {
+    origin: startPoint,
+    destination: end,
+    travelMode: google.maps.TravelMode.DRIVING,
+  };
+  directionsService.route(request, function (response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      callback(response);
+    } else {
+      size--;
+    }
+  });
+}
+
+// Stores a routing result from the API in our global array for routes.
+function storeResult(data: any) {
+  routeResults.push(data);
+  if (routeResults.length === size) {
+    findShortest();
+  }
+}
+
+// Goes through all routes stored and finds which one is the shortest. It then
+// sets the shortest route on the map for the user to see.
+function findShortest() {
+  let i = routeResults.length;
+  let shortestIndex = 0;
+  let shortestLength = routeResults[0].routes[0].legs[0].distance.value;
+
+  while (i--) {
+    if (routeResults[i].routes[0].legs[0].distance.value < shortestLength) {
+      shortestIndex = i;
+      shortestLength = routeResults[i].routes[0].legs[0].distance.value;
+    }
+  }
+  directionsDisplay.setDirections(routeResults[shortestIndex]);
+}
+
+// Marker Utils
 
 // Adds a marker to the map and push to the array.
 function addMarker(
